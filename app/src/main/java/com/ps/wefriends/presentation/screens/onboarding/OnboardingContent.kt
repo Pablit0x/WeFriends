1cc592e36225c5b00d7b18b847f21220aba982a7
noner <pawel495@interia.pl>
2023-10-07 12:45:41 +0100

    Prepering onboarding

---
+++ app/src/main/java/com/ps/wefriends/presentation/screens/onboarding/OnboardingContent.kt
@@ -0,0 +1,8 @@
+package com.ps.wefriends.presentation.screens.onboarding
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OnboardingContent() {
+
+}
---
+++ app/src/main/java/com/ps/wefriends/presentation/screens/onboarding/OnboardingViewModel.kt
@@ -0,0 +1,8 @@
+package com.ps.wefriends.presentation.screens.onboarding
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class OnboardingViewModel : ViewModel() {
+}
