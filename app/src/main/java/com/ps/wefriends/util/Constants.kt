a6212166823bdaf508e1f1c038b4c96ed7c88e21
noner <pawel495@interia.pl>
2023-10-09 14:59:13 +0100

    Updating Firebase Client ID

--- app/src/main/java/com/ps/wefriends/util/Constants.kt
+++ app/src/main/java/com/ps/wefriends/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.ps.wefriends.util
 
 object Constants {
-    const val CLIENT_ID = "142603986745-9dnst8cvuci59u5gkqjhr1fnk32q7539.apps.googleusercontent.com"
+    const val CLIENT_ID = "285343041070-q1sonq1346ga9v2iqqf009c88vbev0pr.apps.googleusercontent.com"
     const val USER_INFO_FILE = "user_info.json"
 }
